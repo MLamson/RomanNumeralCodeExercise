92d01d76abeeb6dfd1bea8bee7199a15fcb1731f
mlamson <mark_lamson@yahoo.com>
2016-07-06 02:42:17 -0400

    Have a working program.  Needs to be refactored.

---
+++ RomanNumeralsCodeExercise/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsCodeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        internal static string ConvertArabicToRoman(int arabicNumber)
+        {
+            var output = "";
+            while (arabicNumber >= 1000)
+            {
+                output += "M";
+                arabicNumber -= 1000;
+            }
+            if (arabicNumber >= 900)
+            {
+                output += "CM";
+                arabicNumber -= 900;
+            }
+            if (arabicNumber >= 500)
+            {
+                output += "D";
+                arabicNumber -= 500;
+            }
+            if (arabicNumber >= 400)
+            {
+                output += "CD";
+                arabicNumber -= 400;
+            }
+            while (arabicNumber >= 100)
+            {
+                output += "C";
+                arabicNumber -= 100;
+            }
+            if (arabicNumber >= 90)
+            {
+                output += "XC";
+                arabicNumber -= 90;
+            }
+            if (arabicNumber >= 50)
+            {
+                output += "L";
+                arabicNumber -= 50;
+            }
+            if (arabicNumber >= 40)
+            {
+                output += "XL";
+                arabicNumber -= 40;
+            }
+            while (arabicNumber >= 10)
+            {
+                output += "X";
+                arabicNumber -= 10;
+            }
+            if (arabicNumber == 9)
+            {
+                output += "IX";
+                arabicNumber -= 9;
+            }
+            if (arabicNumber >= 5)
+            {
+                output += "V";
+                arabicNumber -= 5;
+            }
+            if (arabicNumber == 4)
+            {
+                output += "IV";
+                arabicNumber -= 4; 
+            }
+            while (arabicNumber >= 1)
+            {
+                output += "I";
+                arabicNumber--;
+            }
+              
+            return output;
+        }
+    }
+}
---
+++ RomanNumeralsCodeExercise/RomanNumeralsTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsCodeExercise
+{
+    [TestFixture]
+    class RomanNumeralsTests
+    {
+        [TestCase("I", 1)]
+        [TestCase("II", 2)]
+        [TestCase("III", 3)]
+        [TestCase("IV", 4)]
+        [TestCase("V", 5)]
+        [TestCase("VI", 6)]
+        [TestCase("VIII", 8)]
+        [TestCase("IX", 9)]
+        [TestCase("X", 10)]
+        [TestCase("XIII", 13)]
+        [TestCase("XVII", 17)]
+        [TestCase("XXXI", 31)]
+        [TestCase("L", 50)]
+        [TestCase("LI", 51)]
+        [TestCase("LXVIII", 68)]
+        [TestCase("LXXXVIII", 88)]
+        [TestCase("LXXX", 80)]
+        [TestCase("LXXXIX", 89)]
+        [TestCase("C", 100)]
+        [TestCase("XC", 90)]
+        [TestCase("CXCIX", 199)]
+        [TestCase("CC", 200)]
+        [TestCase("CCLXXXIX", 289)]
+        [TestCase("CCXCVIII", 298)]
+        [TestCase("CCXCIX", 299)]
+        [TestCase("CCC", 300)]
+        [TestCase("D", 500)]
+        [TestCase("CCCXCIX", 399)]
+        [TestCase("CD", 400)]
+        [TestCase("CDXCIX", 499)]
+        [TestCase("DC", 600)]
+        [TestCase("DCXLIX", 649)]
+        [TestCase("M", 1000)]
+        [TestCase("MMM", 3000)]
+        [TestCase("MMCMXCIX", 2999)]
+        [TestCase("MMCDXLIV", 2444)]
+        public void ArabicOne_To_Roman_Numeral(string roman, int arabic)
+        {
+            // Act
+            string result = Program.ConvertArabicToRoman(arabic);
+
+            // Assert
+            Assert.AreEqual(roman, result);
+        }
+    }
+}
