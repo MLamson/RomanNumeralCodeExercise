3b2cd8017b9ff14ce2bfce4cb96bcfa1082a26bb
mlamson <mark_lamson@yahoo.com>
2016-07-10 16:52:13 -0400

    Changed 2 Arrays to 1 Dictionary

--- RomanNumeralsCodeExercise/Program.cs
+++ RomanNumeralsCodeExercise/Program.cs
@@ -17,22 +17,19 @@ namespace RomanNumeralsCodeExercise
             int currentValue = arabicNumber;
             var output = "";
 
-            int[] arabicValues =
+            Dictionary<int, string> arabicRomanEquivalance = new Dictionary<int, string>
             {
-                1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
-            };
-            string[] romanValues =
-            {
-                "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V",
-                "IV", "I"
+                {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+                {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+                {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I" }
             };
 
-            for (int i = 0; i < arabicValues.Length; i++)
+            for (int i = 0; i < arabicRomanEquivalance.Count; i++)
             {
-                while (currentValue >= arabicValues[i])
+                while (currentValue >= arabicRomanEquivalance.ElementAt(i).Key)
                 {
-                    output += romanValues[i];
-                    currentValue -= arabicValues[i];
+                    output += arabicRomanEquivalance.ElementAt(i).Value;
+                    currentValue -= arabicRomanEquivalance.ElementAt(i).Key;
                 }
             }
             
